890cf272730536107d5040bd28dd0c807807d8c6
jamesinaxx <grassojames5@gmail.com>
2021-04-21 14:32:37 +1000

    bump

--- src/main/java/com/jamesinaxx/fomo/discord/Events.java
+++ src/main/java/com/jamesinaxx/fomo/discord/Events.java
@@ -1,4 +1,17 @@
 package com.jamesinaxx.fomo.discord;
 
-public class Events {
+import com.jamesinaxx.fomo.FOMO;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.bukkit.Bukkit;
+
+public class Events extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getChannel().getName() == FOMO.config.getString("channel")) {
+            Bukkit.broadcastMessage("[Discord/" + event.getAuthor().getAsTag() + "] " + event.getMessage().getContentRaw());
+        }
+    }
+
 }
