6f8eb57f3dfd28045b9bb46d88f87da8cf316051
jamesinaxx <grassojames5@gmail.com>
2021-04-28 15:28:13 +1000

    Fixed discord class

--- src/main/java/com/jamesinaxx/fomo/Discord.java
+++ src/main/java/com/jamesinaxx/fomo/Discord.java
@@ -11,7 +11,7 @@ import static com.jamesinaxx.fomo.FOMO.*;
 public class Discord extends ListenerAdapter {
 
     @Override
-    public void onMessageRecieved(MessageReceivedEvent event) {
+    public void onMessageReceived(MessageReceivedEvent event) {
         String prefix = Objects.requireNonNull(config.getString("bot.prefix"));
         if (event.getChannel().getIdLong() == FOMO.config.getLong("bot.channel") && !event.getAuthor().isBot()) {
             Bukkit.broadcastMessage("<Discord/" + event.getAuthor().getAsTag() + "> " + event.getMessage().getContentRaw());
@@ -20,7 +20,7 @@ public class Discord extends ListenerAdapter {
 
     public static void sendMessage(String msg) {
         if (channel != null && client != null) {
-            channel.createMessage(msg).block();
+            channel.sendMessage(msg).queue();
         }
     }
 }
